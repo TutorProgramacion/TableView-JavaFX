3b2c2e9915ad3359c414a310d06fa2d3463907ca
Carmelo Marín Abrego <Carmelo Marn Abrego@Carmelo>
2015-08-19 14:38:06 -0500

    Tutorial uso de TableView en JavaFX 8

---
+++ tutor-programacion/src/tutor/programacion/Persona.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutor.programacion;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Carmelo Marín Abrego
+ */
+public class Persona {
+
+    private String nombre;
+    private String apellido;
+    private LocalDate fechaNacimiento;
+
+    public Persona(String nombre, String apellido, LocalDate fechaNacimiento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+}
---
+++ tutor-programacion/src/tutor/programacion/Principal.java
@@ -0,0 +1,59 @@
+package tutor.programacion;
+
+import java.time.LocalDate;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Carmelo Marín Abrego
+ */
+public class Principal extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        TableView<Persona> tableView = new TableView();
+        TableColumn<Persona, String> colNombre = new TableColumn<>("Nombre");
+        TableColumn<Persona, String> colApellido = new TableColumn<>("Apellido");
+        TableColumn<Persona, String> colPersona = new TableColumn<>("Persona");
+        colPersona.getColumns().addAll(colNombre, colApellido);
+        
+        TableColumn<Persona, LocalDate> colFechaNacimiento = new TableColumn<>("Fecha de Nacimiento");
+        tableView.getColumns().addAll(colPersona, colFechaNacimiento);
+
+        colNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        colApellido.setCellValueFactory(new PropertyValueFactory<>("apellido"));
+        colFechaNacimiento.setCellValueFactory(new PropertyValueFactory<>("fechaNacimiento"));
+
+        Persona p1 = new Persona("Juan", "Perez", LocalDate.parse("2012-11-01"));
+        Persona p2 = new Persona("Maria", "Loza", LocalDate.parse("2013-01-15"));
+        Persona p3 = new Persona("Adriana", "Mendez", LocalDate.parse("2014-07-20"));
+
+        tableView.getItems().addAll(p1, p2, p3);
+        tableView.setTableMenuButtonVisible(true);
+        tableView.setPlaceholder(new Label("El TableView no contiene datos."));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(tableView);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("TableView - Tutor de Programación");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
